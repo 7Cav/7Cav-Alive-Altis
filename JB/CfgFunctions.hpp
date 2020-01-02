bee7ee2c6f7114210267976e6695b1ca5e07e987
7CavArma3Dev <59429466+7CavArma3Dev@users.noreply.github.com>
2020-01-02 05:09:31 +0000

    Add files via upload

---
+++ JB/CfgFunctions.hpp
@@ -0,0 +1,13 @@
+class JB {
+	tag = "JB";
+	class functions {
+		file = "JB";
+		class displayName {};				//greenzone related
+		class showBlackScreenMessage {};	//greenzone related
+		class respawnVehicleWhenAbandoned {};		//vehicle respawns
+		class respawnVehicleWhenKilled {};		//vehicle respawns
+		class respawnVehicleInitialize {};		//vehicle respawns
+		class respawnVehiclePreInit { preInit = 1; };
+		class respawnVehicleReturn {};
+	};
+};
---
+++ scripts/vehicle/CfgFunctions.hpp
@@ -0,0 +1,13 @@
+class vehicle
+{
+	tag = "vehicle";
+	class functions
+	{
+		file = "vehicle";
+		class respawnVehicleInitialize {};
+		class respawnVehiclePreInit {};
+		class respawnVehicleReturn {};
+		class respawnVehicleWhenAbandoned {};
+		class respawnVehicleWhenKilled {};
+	};
+};
