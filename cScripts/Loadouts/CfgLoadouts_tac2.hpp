53fa538e2cd0f84978dcaa9eb2400bee9dae85b3
David Citarelli <davidcit646@gmail.com>
2020-01-03 01:18:59 -0500

    Fixed syntax

--- cScripts/Loadouts/CfgLoadouts_tac2.hpp
+++ cScripts/Loadouts/CfgLoadouts_tac2.hpp
@@ -342,7 +342,8 @@ class tac2_Mustang : tac2_base {
        "ACE_surgicalKit",1,
        "ACE_PersonalAidKit",4,
        "ACE_tourniquet",8,
-       "ACE_salineIV",8
+       "ACE_salineIV",
+       };
     preLoadout = "[(_this select 0), 'charlie', 0, 0] call cScripts_fnc_setPreInitPlayerSettings;";
     postLoadout = "[(_this select 0),true,true] call cScripts_fnc_setPostInitPlayerSettings;";
     };
